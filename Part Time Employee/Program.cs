25759442fd139128c87b8ddba0c8ca1346076a6b
tahsinraza95 <tahsinr693@gmail.com>
2023-02-01 01:06:29 +0530

    Added: Part Time Employee Wage

---
+++ Part Time Employee/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Part_Time_Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random= new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                empHrs= 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
+        }
+    }
+}
